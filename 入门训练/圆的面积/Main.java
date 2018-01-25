729b674b44477f10d1ad6e902f51c611a9611e61
linzhennan <598296448@qq.com>
2018-01-25 16:55:38 +0800

    入门训练完成

---
+++ 入门训练/圆的面积/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         double PI=3.14159265358979323;
+         Scanner sc=new Scanner(System.in);
+         int r=sc.nextInt();
+         System.out.printf("%.7f",r*r*PI);
+    }
+}
---
+++ 入门训练/序列求和/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         int n=new Scanner(System.in).nextInt();
+         System.out.println((n+(long)n*n)/2);
+    }
+}
