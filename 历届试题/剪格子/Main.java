41f9c0ecc8f9ffc3a6086f515beaaf4b865183f5
linzhennan <598296448@qq.com>
2018-03-05 00:18:02 +0800

    增加判断是否联系区域 依旧有问题

--- 历届试题/剪格子/Main.java
+++ 历届试题/剪格子/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 //有错误
 public class Main {
     int[][] path = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-    int minstep = 99, sum = 0;
+    int minstep = 999, sum = 0;
 
     public static void main(String[] args) {
         //录入数据
@@ -24,6 +24,7 @@ public class Main {
 //                System.out.print(a[i][j]+"  \t");
 //            System.out.println();
 //        }
+    
     }
 
     private int min(int[][] a) {
@@ -50,7 +51,55 @@ public class Main {
         } else
             return 0;
     }
-
+    
+    //广度检查是否形成连续区域
+    private boolean islianxvqvyv(int[][] a){
+        int x=0,y=0;
+      //找出没有标记的点
+        boolean b=false;
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++)
+                if(0==a[i][j]){
+                    x=i;
+                    y=j;
+                    b=true;
+                    break;
+                }
+        if(b)
+            break;
+        }
+        
+        List<point> list=new ArrayList<point>();
+//        int[][] book=a.clone();
+        //克隆
+        int[][] book=new int[a.length][a[0].length];
+        for(int i=0;i<a.length;i++)
+            for(int j=0;j<a[0].length;j++)
+                if(1==a[i][j])
+                    book[i][j]=a[i][j];
+        
+        book[x][y]=1;
+        list.add(new point(x,y));
+        //染色
+        for(int i=0;i<list.size();i++){
+            for(int j=0;j<4;j++){
+                int _x=list.get(i).x+path[j][0],_y=list.get(i).y+path[j][1];
+                if (_x < a.length && _y < a[0].length && _x >= 0 && _y >= 0) {
+                    if(0==book[_x][_y]){
+                        book[_x][_y]=1;
+                        list.add(new point(_x,_y));
+                    }
+                }
+            }
+        }
+        //是否存在没染色的点
+        for(int i=0;i<a.length;i++)
+            for(int j=0;j<a[0].length;j++)
+                if(0==book[i][j])
+                    return false;
+        return true;
+        
+    }
     //深度
     private void minlength(int[][] a, int[][] book, int step, int x, int y, int target, int[][] re_a) {
         if (sum < target) {
@@ -68,22 +117,22 @@ public class Main {
             }
         } else {
             if (sum == target) {
-                //计算长度
-                int len = 0;
-                for (int i = 0; i < a.length; i++)
-                    for (int j = 0; j < a[0].length; j++)
-                        if (1 == book[i][j])
-                            len++;
-                //是否最小长度
-                if (len < minstep) {
-                    int[][] _a = new int[a.length][a[0].length];
+                //是否最小长度且连续区域
+                if (--step < minstep&&islianxvqvyv(book)) {
                     for (int i = 0; i < a.length; i++)
                         for (int j = 0; j < a[0].length; j++)
-                            if (1 == book[i][j])
-                                re_a[i][j] = 1;
-                    re_a = _a;
+                                re_a[i][j] = book[i][j];
+                    minstep=step;
                 }
             }
         }
+    }
+}
+class point{
+    public int x;
+    public int y;
+    public point(int a,int b){
+        x=a;
+        y=b;
     }
 }
