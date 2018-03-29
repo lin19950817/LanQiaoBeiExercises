ffa5b0876e89661d2a6c77df8c49af9418836f7b
linzhennan <598296448@qq.com>
2018-03-29 19:59:56 +0800

    动态规划，还是超时。。

--- 算法训练/结点选择/Main.java
+++ 算法训练/结点选择/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-// 超时
+// 超时。。
 public class Main {
     private int maxSum;
     private int n;
@@ -37,6 +37,8 @@ public class Main {
 //        matrix[0][2] = true;matrix[2][0] = true;
 //        matrix[1][3] = true;matrix[3][1] = true;
 //        matrix[1][4] = true;matrix[4][1] = true;
+//        System.out.println(new Main(n, a, matrix).maxValue());
+//        
 //        
 //        int n1 = 10;
 //        int[] a1 = {41, 982, 686, 796, 781, 801, 63, 52, 60, 748};
@@ -50,6 +52,8 @@ public class Main {
 //        matrix1[0][2] = true;matrix1[2][0] = true;
 //        matrix1[1][6] = true;matrix1[6][1] = true;
 //        matrix1[3][4] = true;matrix1[4][3] = true;
+//        System.out.println(new Main(n1, a1, matrix1).maxValue());
+//        
 //        
 //        int n2 = 20;
 //        int[] a2 = {341, 697, 481, 697, 210, 129, 172, 214, 13, 801, 733, 445, 701, 731, 11, 946, 145, 717, 753, 618};
@@ -73,9 +77,6 @@ public class Main {
 //        matrix2[12][5] = true;matrix2[5][12] = true;
 //        matrix2[7][19] = true;matrix2[19][7] = true;
 //        matrix2[9][11] = true;matrix2[11][9] = true;
-//        
-//        System.out.println(new Main(n, a, matrix).maxValue());
-//        System.out.println(new Main(n1, a1, matrix1).maxValue());
 //        System.out.println(new Main(n2, a2, matrix2).maxValue());
         
         
@@ -87,83 +88,89 @@ public class Main {
         maxSum = 0;
     }
     private int maxValue() {
-//        search(new boolean[n], 0, n);
+        // 将a数组放进notes
+        Point[] notes = new Point[n];
+        for (int i = 0; i < n; i++) {
+            notes[i] = new Point(i, a[i]);
+        }
         
-        search2(new boolean[n], 0, 0);
+        int maxValueOfSelected = notes[0].getMaxValueOfSelected(matrix, new boolean[n], notes), maxValueOfUnselected = notes[0].getMaxValueOfUnselected(matrix, new boolean[n], notes);
+        this.maxSum = maxValueOfSelected > maxValueOfUnselected ? maxValueOfSelected : maxValueOfUnselected;
+        
         return maxSum;
     }
-    /** 
-     * 暴力枚举
-     * 参数：cannotBeSelected：标记不能被选择的节点，sum：当前值总和，canSelectCount：剩余能被选择的节点数
-     * */
-    private void search(boolean[] cannotBeSelected, int sum, int canSelectCount) {
-        if (0 == canSelectCount) {
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (!cannotBeSelected[i]) {
-                boolean[] newBook = cannotBeSelectedClone(cannotBeSelected);
-                int next = nextCannotBeSelectedCount(newBook, i);
-                
-                search(newBook, sum + a[i], canSelectCount - next);
-                
-                
-            }
-        }
+}
+class Point{
+    private int subscript;
+    private int value;
+    private int maxValueOfSelected;
+    private int maxValueOfUnselected;
+    public int getValue() {
+        return value;
     }
-    private void search2(boolean[] cannotBeSelected, int sum, int step) {
-        if (n == step) {
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
-            return;
-        }
-        if (!cannotBeSelected[step]) {
-            for (int i = 0; i < 2; i++) {
-                boolean[] newBook = cannotBeSelectedClone(cannotBeSelected);
-                // 标记
-                if (0 == i) {
-                    // 选择点后，与其相连的点都不能被选择
-                    newBook[step] = true;
-                    for (int j =0; j < n; j++) {
-                        if (matrix[step][j] && !cannotBeSelected[j]) {
-                            newBook[j] = true;
-                        }
-                    } 
-                }
-                search2(newBook, newBook[step] ? sum + a[step] : sum, step + 1);
-            }
-        } else {
-            search2(cannotBeSelected, sum, step + 1);
-        }
+    public void setValue(int value) {
+        this.value = value;
     }
-    /** 
-     * 克隆cannotBeSelectedClone
-     * */
-    private boolean[] cannotBeSelectedClone(boolean[] cannotBeSelected) {
-        boolean[] newArray = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            newArray[i] = cannotBeSelected[i];
+    public int getMaxValueOfSelected(boolean[][] matrix, boolean[] book, Point[] notes) {
+        if (maxValueOfSelected != -1) {
+            return maxValueOfSelected;
         }
-        return newArray;
+        
+        book[subscript] = true;
+        setMaxValueOfSelected(matrix, book, notes);
+        return maxValueOfSelected;
     }
     /**
-     * 计算下一个不能被选择的节点的数量
-     * 参数：cannotBeSelected：标记不能被选择的节点（下个不能被选择的节点会被标记），selectedPoint：当前选择的点
+     * 被选择最大值为所有连接点的不能被选择最大值的和
      * */
-    private int nextCannotBeSelectedCount(boolean[] cannotBeSelected, int selectedPoint) {
-        // 当前选择的点标记上
-        int sum = 1;
-        cannotBeSelected[selectedPoint] = true;
-        for (int i = 0; i < n; i++) {
-            if (matrix[selectedPoint][i] && !cannotBeSelected[i]) {
-                cannotBeSelected[i] = true;
-                sum++;
+    public void setMaxValueOfSelected(boolean[][] matrix, boolean[] book, Point[] notes) {
+        int sum = 0, length = matrix.length;
+        for (int i = 0; i < length; i++) {
+            if (!book[i] && matrix[subscript][i]) {
+                sum += notes[i].getMaxValueOfUnselected(matrix, book, notes);
             }
         }
-        return sum;
+        this.maxValueOfSelected = sum + value;
+    }
+    public int getMaxValueOfUnselected(boolean[][] matrix, boolean[] book, Point[] notes) {
+        if (maxValueOfUnselected != -1) {
+            return maxValueOfUnselected;
+        }
+        
+        book[subscript] = true;
+        setMaxValueOfUnselected(matrix, book, notes);
+        return maxValueOfUnselected;
+    }
+    /**
+     * 不能被选择最大值为所有连接点的被选择最大值和不能被选择最大值之间最大数的和
+     * */
+    public void setMaxValueOfUnselected(boolean[][] matrix, boolean[] book, Point[] notes) {
+        int sum = 0, length = matrix.length;
+        boolean[] anotherBook = bookClone(book);
+        for (int i = 0; i < length; i++) {
+            if (!book[i] && matrix[subscript][i]) {
+                int nextMaxValueOfSelected = notes[i].getMaxValueOfSelected(matrix, book, notes), nextMaxValueOfUnselected = notes[i].getMaxValueOfUnselected(matrix, anotherBook, notes);
+                sum += nextMaxValueOfSelected > nextMaxValueOfUnselected ? nextMaxValueOfSelected : nextMaxValueOfUnselected;
+            }
+            
+        }
+        this.maxValueOfUnselected = sum;
+    }
+    public Point(int subscript, int value) {
+        this.subscript = subscript;
+        this.value = value;
+        maxValueOfSelected = -1;
+        maxValueOfUnselected = -1;
+    }
+    
+    private boolean[] bookClone(boolean[] book) {
+        int length = book.length;
+        boolean[] newBook = new boolean[length];
+        for (int i = 0; i < length; i++) {
+            if (book[i]) {
+                newBook[i] = true;
+            }
+        }
+        return newBook;
     }
 }
